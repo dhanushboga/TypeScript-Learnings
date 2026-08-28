58aa571108acb152cbd14a4b7d8102a90c6bf82a
Boga <bogac@science.regn.net>
2026-08-28 11:50:39 +0100

    message

---
+++ demo.js
@@ -0,0 +1,5 @@
+function add(a,b) {
+    return a+b;
+}
+let result = add(10,50);
+console.log(result);
--- javascript.js
+++
@@ -1,5 +0,0 @@
-function add(a,b){
-    return a+b;
-}
-let result = add(5,'hello');
-console.log(result);
