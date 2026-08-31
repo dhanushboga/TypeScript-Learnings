0063b34230c3470c5436ebc838442b5e8c63fe1c
dhanushboga <dhanushboga555@gmail.com>
2026-08-31 15:13:26 +0530

    message

---
+++ variables.ts
@@ -0,0 +1,15 @@
+//variables
+let course : string = "TypeScript"
+let price: number = 990;
+let isCompleted: boolean = false;
+
+course = "javascript"
+
+//any
+let value : any = 300
+value = "dhanush"
+value = [];
+value = {};
+
+//unknown
+//unknown is safe than 'any'
